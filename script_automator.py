5aecf841520455d32fd62126e27a2a391124a732
EgbieAnderson1 <jayunderwood2011@hotmail.com>
2015-08-05 17:02:54 +0100

    Update script_automator.py

--- script_automator.py
+++ script_automator.py
@@ -15,6 +15,8 @@
 # in option 1 to an existing script. It does so without altering the data 
 # that already exists within that script.
 # 
+# Do not option 1 with a file that contains data it will delete the
+# data instead use option 2
 ########################################################################
 
 import webbrowser
