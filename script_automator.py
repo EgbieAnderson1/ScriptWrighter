c9dbe6ba2a2b582d4908b11b383c77150d7a6b7c
EgbieAnderson1 <jayunderwood2011@hotmail.com>
2015-08-06 13:42:09 +0100

    Update script_automator.py

--- script_automator.py
+++ script_automator.py
@@ -44,7 +44,7 @@ class Description(object):
         if len(message) < 80:
             return False, message
 
-        # ensure that length of the characters does not exceed 80 chars
+        # ensure that length of the characters does not exceed 80 chars for each new line
         while len(message) > 80:
             text.append(message[:80])
             message = message[80:]
@@ -55,11 +55,11 @@ class Description(object):
 class Format(object):
     """A simple class creates the format for the page"""
 
-    def get_border(self, num):
+    def draw_border(self, num):
         return "#" * num
         
-class InputOutput(Format):
-    """Allows the user to input data to a text file"""
+class FileInputOutput(Format):
+    """Class controls what goes in and out of a file"""
 
     def __init__(self, file_name, loc):
         
@@ -67,7 +67,7 @@ class InputOutput(Format):
         self.create_new_file(self._file_location)                  # create a file at that location
         self.add_string('#!/usr/bin/python')                   # add a shebang to the top of the file
         self.add_string('\n')
-        self.add_string(self.get_border(80))			   # add a border to the top of that page
+        self.add_string(self.draw_border(80))			   # add a border to the top of that page
 
     def load(self, file_name):
         """load an existing file"""
@@ -88,26 +88,26 @@ class InputOutput(Format):
         f.write('\n')
         f.close()
 
-    def get_user_input(self):
-        """Get input from the user"""
+def get_user_input():
+    """Get input from the user"""
 
-        title, script_description, version, author = False, False, False, False
+    title, script_description, version, author = False, False, False, False
 
-        while True:
+    while True:
 
-            print
-            if not author:
-                author = raw_input("[+] Enter the author for the file : ")
-            if not title:
-                title = raw_input("[+] Enter name of the script : ")
-            if not script_description:
-                script_description = raw_input("[+] Enter the description for the script : ")
-            if not version:
-                version = raw_input("[+] Enter the version number : ")
-            if title and script_description and version and author:
-                break
+        print
+        if not author:
+            author = raw_input("[+] Enter the author for the file : ")
+        if not title:
+            title = raw_input("[+] Enter name of the script : ")
+        if not script_description:
+            script_description = raw_input("[+] Enter the description for the script : ")
+        if not version:
+            version = raw_input("[+] Enter the version number : ")
+        if title and script_description and version and author:
+            break
 
-        return title, script_description, version, author
+    return title, script_description, version, author
             
 def create_template(user_obj):
     """ create_template(obj) -> return (None)
@@ -117,33 +117,37 @@ def create_template(user_obj):
     info = Description()
     time = Time()
     border = Format()
-    title, description, version, author = user_obj.get_user_input()
-    user_obj.add_string(border.get_border(1))
+    title, description, version, author = get_user_input()
+    user_obj.add_string(border.draw_border(1))
 
-    time_str = "{} Created on the {} at {} hrs".format(border.get_border(1), time.get_date(), time.get_time())
-    author_name = "{} Created by : {} ".format(border.get_border(1), author)
-    script_name = "{} Name of the script : {} ".format(border.get_border(1), title)
-    ver = "{} This is version : {} ".format(border.get_border(1), version)
+    # create a string with following information
+    time_str = "{} Created on the {} at {} hrs".format(border.draw_border(1), time.get_date(), time.get_time())
+    author_name = "{} Created by : {} ".format(border.draw_border(1), author)
+    script_name = "{} Name of the script : {} ".format(border.draw_border(1), title)
+    ver = "{} This is version : {} ".format(border.draw_border(1), version)
     val, script_description = info.get_script_description(description)
 
+    # write the string to file
     user_obj.add_string(author_name.title())
     user_obj.add_string(script_name.title())
     user_obj.add_string(time_str)
     user_obj.add_string(ver)
-    user_obj.add_string(border.get_border(1))
-    user_obj.add_string(border.get_border(1))
-    user_obj.add_string("{} File description ".format(border.get_border(1)))
-    user_obj.add_string(border.get_border(1))
+    user_obj.add_string(border.draw_border(1))
+    user_obj.add_string(border.draw_border(1))
+    user_obj.add_string("{} File description ".format(border.draw_border(1)))
+    user_obj.add_string(border.draw_border(1))
 
+    # write the main text to file
     if not val:
-        user_obj.add_string("{} {}".format(border.get_border(1), script_description))
+        user_obj.add_string("{} {}".format(border.draw_border(1), script_description))
 
     else:
         for text in script_description:
-            user_obj.add_string("{} {}".format(border.get_border(1), text))
+            user_obj.add_string("{} {}".format(border.draw_border(1), text))
 
-    user_obj.add_string(border.get_border(1))
-    user_obj.add_string(border.get_border(80))
+    # finish of by creating the border
+    user_obj.add_string(border.draw_border(1))
+    user_obj.add_string(border.draw_border(80))
 
 
 def display(f):
@@ -163,26 +167,28 @@ def main():
                               help="edit existing file")
     options, args = parser.parse_args()
 
+    # option 1
     # adds information to a new file.
-    if (options.filename and options.location): 
+    if (options.filename and options.location) and not options.add: 
         if not os.path.exists(options.location) and not os.path.isdir(options.location):
             print "[+] The location for the file is not valid. Enter a valid file location"
             exit(0)
 
-        user = InputOutput(options.filename, options.location)
+        user = FileInputOutput(options.filename, options.location)
         create_template(user) # create the template
         display(user._file_location)
 
+    # option 2
     # Enables information to be added to the top of a file or script that already
     # has information without overiding the information that already exists in the file.
     elif not options.location and options.filename and options.add:
         if os.path.exists(options.filename):
 
             file_path = os.path.dirname(options.filename)
-            user = InputOutput("log.txt", file_path)
-            
+            user = FileInputOutput("log.txt", file_path)
             old_file = user.load(options.filename)
-            user = InputOutput(options.filename, file_path)
+            user.add_string('[+] Successful copied the content of the old file')
+            user = FileInputOutput(options.filename, file_path)
             create_template(user)
             user.add_string('\n')
             user.add_string(old_file)
